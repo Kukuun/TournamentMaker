4315137c15d37cb5da0f84fd9ac0bf46bdf65821
Kukuun <pmunk87@gmail.com>
2017-10-11 23:46:52 +0200

    Added a ViewModelBase class.

---
+++ Tournament/Tournament.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Tournament.UI.ViewModels {
+    public class ViewModelBase : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // The CallerMemberName attribute eliminates the need of hardcoded strings.
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
